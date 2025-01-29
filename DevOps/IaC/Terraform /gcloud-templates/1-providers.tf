7dcde8a26fd3d77c8c6e890ee05ba032ddb66a79
s.stoyanov <sergeycrowler@gmail.com>
2025-01-29 12:31:00 +0200

    update providers

--- DevOps/IaC/Terraform /gcloud-templates/1-providers.tf
+++ DevOps/IaC/Terraform /gcloud-templates/1-providers.tf
@@ -1,12 +1,47 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "google" {
   project = local.project_id
   region  = local.region
 }
 
-terraform {
-  required_version = ">== 1.0"
-  required_providers {
-    source = "hashicorp/google"
-    version = "~> 6.0"
+data "google_client_config" "current" {}
+
+data "google_container_cluster" "primary" {
+  name     = "your-cluster-name"
+  location = local.region
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.primary.endpoint}"
+  token = data.google_client_config.current.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    host  = "https://${data.google_container_cluster.primary.endpoint}"
+    token = data.google_client_config.current.access_token
+    cluster_ca_certificate = base64decode(
+      data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+    )
   }
 }
